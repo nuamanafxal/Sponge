02bd2777a60e6a556af81899c4d6a8c44a60c1cd
nuaman Afzal <nuaman36@gmail.com>
2022-10-28 23:51:03 -0700

    Laste Chane

---
+++ include/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Sponge Agency</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" type="image/x-icon" href="images/header/favicom.png">
+      <link href="assets/bootstrap5.min.css" rel="stylesheet">
+      <link href="assets/style.css" rel="stylesheet">
+      <link href="assets/w3.css" rel="stylesheet">
+      <link rel="stylesheet" href="assets/aos.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <script src="assets/jquery-3.6.min.js"></script>
+      <script src="assets/bootstrap5.bundle.min.js"></script>
+      <script src="assets/aos.js"></script>
+   </head>
